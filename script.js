6d46e301efc2005394b55565fb0643fcff95d6f9
Julio Cesar <64658627+JuliioCesar@users.noreply.github.com>
2022-03-31 01:00:54 -0300

    criando um elemento button para poder excluir os outros elementos

--- script.js
+++ script.js
@@ -3,13 +3,22 @@ let arr = new Array()
 function criarElemento() {
 
     const valorInput = document.getElementById("text").value
+
     const divMostrar = document.querySelector(".mostrar")
-
+    
+    const buttonExcluir = document.createElement('button')
+    buttonExcluir.innerHTML = 'Excluir'
     const paragrafo = document.createElement("p")
-
+    
+    paragrafo.innerHTML = buttonExcluir
     paragrafo.innerHTML = valorInput
-
+    
+    paragrafo.appendChild(buttonExcluir)
     divMostrar.appendChild(paragrafo)
+
+    buttonExcluir.onclick = function () {
+        paragrafo.remove()
+    }
 
     return valorInput
 }
