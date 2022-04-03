6f29fd15fc79f13983ff1a4f8d130af83732fecb
Julio Cesar <64658627+JuliioCesar@users.noreply.github.com>
2022-04-03 11:19:04 -0300

    botao checkbox e funçao adicionando

--- script.js
+++ script.js
@@ -2,31 +2,43 @@ let arr = new Array()
 
 function criarElemento() {
 
-    const valorInput = document.getElementById("text").value
-
+    const valorInput = document.querySelector(".text").value
+    
     const divMostrar = document.querySelector(".mostrar")
     
+    const divContainer = document.createElement("div")
+    const buttonCheck = document.createElement('input')
+    buttonCheck.setAttribute("type", "checkbox")
+
     const buttonExcluir = document.createElement('button')
-    buttonExcluir.innerHTML = 'Excluir'
+    buttonExcluir.innerHTML= "Excluir"
+
+    buttonCheck.addEventListener("click", () => {
+        paragrafo.style.textDecoration = "line-through"
+    })
+    
     const paragrafo = document.createElement("p")
+
     
-    paragrafo.innerHTML = buttonExcluir
+    paragrafo.innerHTML = buttonCheck
     paragrafo.innerHTML = valorInput
-    
-    paragrafo.appendChild(buttonExcluir)
-    divMostrar.appendChild(paragrafo)
 
     buttonExcluir.onclick = function () {
+        divContainer.remove()
         paragrafo.remove()
     }
 
+    divContainer.appendChild(buttonExcluir)
+    paragrafo.append(buttonCheck)
+    divMostrar.appendChild(paragrafo)
+    divMostrar.appendChild(divContainer)
+    
     return valorInput
 }
+
 function adicionarBloco() {
 
     let resultado = criarElemento()
-
     arr.push(resultado)
-
     console.log(arr)
 }
